0579b7528f04f6626b6678c9a0eef8b00ec09c76
tushar <tusharkanti647@gmail.com>
2023-02-11 16:29:42 +0000

    som portion done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Index from '../Pages/Index';
 
 
-export const NavBar = () =>{
-    return(
-
+export const NavBar = () => {
+    return (
+        <nav>
+            <Link to="/" class="index-link">Index</Link>
+            <Link to="/home" class="home-link">Home</Link>
+        </nav>
     )
 }
